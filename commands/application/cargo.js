4f49750e2f1cf3931aeabbb1eb063708bf0c9f26
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-05-25 19:45:52 +0200

    Update cargo.js

--- commands/application/cargo.js
+++ commands/application/cargo.js
@@ -11,7 +11,7 @@ module.exports = {
     .addStringOption(character =>
   		character.setName('character')
         .setAutocomplete(true)
-  			.setDescription('The character name (e.g. Kyo, Iori).')
+  			.setDescription('The character name (e.g. Terry, B. Jenet).')
   			.setRequired(true))
     .addStringOption(move =>
   		move.setName('move')
@@ -25,7 +25,7 @@ module.exports = {
       const id = interaction.options.getString('move');
       
       // Fetch the cargo data with the appropriate moveId
-      const url_cargo = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOF02UM%2C&&fields=MoveData_KOF02UM.input%2C+MoveData_KOF02UM.input2%2C+MoveData_KOF02UM.name%2C+MoveData_KOF02UM.version%2C+MoveData_KOF02UM.hitboxes%2C+MoveData_KOF02UM.images%2C+MoveData_KOF02UM.damage%2C+MoveData_KOF02UM.guard%2C+MoveData_KOF02UM.startup%2C+MoveData_KOF02UM.active%2C+MoveData_KOF02UM.recovery%2C+MoveData_KOF02UM.hitadv%2C+MoveData_KOF02UM.blockadv%2C+MoveData_KOF02UM.invul%2C+MoveData_KOF02UM.cancel%2C+MoveData_KOF02UM.idle%2C+MoveData_KOF02UM.rank%2C+MoveData_KOF02UM.idle%2C&where=moveId+%3D+%22"+encodeURIComponent(id)+"%22&order+by=&limit=100&format=json";
+      const url_cargo = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_COTW%2C&&fields=MoveData_COTW.input%2C+MoveData_COTW.input2%2C+MoveData_COTW.name%2C+MoveData_COTW.version%2C+MoveData_COTW.hitboxes%2C+MoveData_COTW.images%2C+MoveData_COTW.damage%2C+MoveData_COTW.guard%2C+MoveData_COTW.startup%2C+MoveData_COTW.active%2C+MoveData_COTW.recovery%2C+MoveData_COTW.hitadv%2C+MoveData_COTW.blockadv%2C+MoveData_COTW.invul%2C+MoveData_COTW.cancel%2C+MoveData_COTW.idle%2C+MoveData_COTW.rank%2C+MoveData_COTW.revdamage%2C+MoveData_COTW.guardDamage%2C&where=moveId+%3D+%22"+encodeURIComponent(id)+"%22&order+by=&limit=100&format=json";
       const response_cargo = await fetch(url_cargo);
       const cargo = await response_cargo.json();
   
@@ -55,13 +55,15 @@ module.exports = {
       const dmg = this.getHyperLink(moveData['damage']);
       const guard = this.getHyperLink(moveData['guard']);
       const cancel = this.getHyperLink(moveData['cancel']);
+      const revdmg = this.getHyperLink(moveData['revdamage']);
+      const gdmg = this.getHyperLink(moveData['guardDamage']);
 
       // Fetch hitboxes or images if lack of the former.
       let images = (moveData['images'] !== null) ? moveData['images'].toString().trim().split(',') : [];
       let hitboxes = (moveData['hitboxes'] !== null) ? moveData['hitboxes'].toString().trim().split(',') : images;
     
       // Get character link and img for header and thumbnail.
-      const link = 'https://dreamcancel.com/wiki/The_King_of_Fighters_2002_UM/' + encodeURIComponent(character);
+      const link = 'https://dreamcancel.com/wiki/Fatal_Fury:_City_of_the_Wolves' + encodeURIComponent(character);
       const img = this.getCharacterImg(character);
       
       const embeds = [];
@@ -71,7 +73,7 @@ module.exports = {
         .setURL(link)
         .setAuthor({ name: move, iconURL: 'https://pbs.twimg.com/profile_images/1150082025673625600/m1VyNZtc_400x400.png', url: link + '/Data' })
         // .setDescription('Move input')
-        .setThumbnail('https://tiermaker.com/images/chart/chart/the-king-of-fighters-2002-um-characters-137019/64px-portraitkof2002um' + img + 'png.png')
+        .setThumbnail('https://tiermaker.com/images//media/template_images/2024/137019/fatal-fury-city-of-the-wolves-characters-including-dlc-137019/zzzzz-174' + img + '.png')
         .addFields(
           { name: 'Startup', value: startup, inline: true },
           { name: 'Active', value: active, inline: true },
@@ -89,11 +91,14 @@ module.exports = {
           { name: 'On hit', value: oh, inline: true },
           { name: 'On block', value: ob, inline: true },
           { name: '\u200B', value: '\u200B' },
+          { name: 'Rev Damage', value: revdmg, inline: true },
+          { name: 'Guard Damage', value: gdmg, inline: true },
+          { name: '\u200B', value: '\u200B' },
           { name: 'Invincibility', value: inv },
           // { name: 'Inline field title', value: 'Some value here', inline: true },
           )
       }
-        embed.setFooter({ text: 'Got feedback? Join the 02UM server: discord.gg/8JNXHxf', iconURL: 'https://cdn.iconscout.com/icon/free/png-128/discord-3-569463.png' });
+        embed.setFooter({ text: 'Got feedback? Join the COTW server: discord.gg/ChEEUuZwqS', iconURL: 'https://cdn.iconscout.com/icon/free/png-128/discord-3-569463.png' });
         if (hitboxes.length === 0) {
           embed.addField('No image was found for this move', 'Feel free to share with Franck Frost if you have one.', true);
           embeds.push(embed)
@@ -142,53 +147,28 @@ module.exports = {
       return;
       } catch (error) {
         console.log("Error finishing cargo request", error);
-        await interaction.editReply('There was an error while processing your **cargo** request, reach out to <@259615904772521984>. Refer to the [Google sheet](https://docs.google.com/spreadsheets/d/1lzpQMoGAboJezLT9WRd3O-vlNDNRlgF_47ShtBGZ3G4) to look for the data.');
+        await interaction.editReply('There was an error while processing your **cargo** request, reach out to <@259615904772521984>. Refer to the [Google sheet](LINK HERE) to look for the data.');
         return;
       }
   },
   getCharacter: function(character) {
+    if (character.includes('Jenet')) return 'B. Jenet';
     const chart = {
-      'Andy': 'Andy Bogard',
-      'Athena': 'Athena Asamiya',
-      'Benimaru': 'Benimaru Nikaido',
       'Billy': 'Billy Kane',
-      'Mary': 'Blue Mary',
-      'Ex Kensou': 'EX Kensou',
-      'Ex Robert': 'EX Robert',
-      'Ex Takuma': 'EX Takuma',
-      'O.Chris': 'Orochi Chris',
-      'O.Shermie': 'Orochi Shermie',
-      'O.Yashiro': 'Orochi Yashiro',
-      'Chang': 'Chang Koehan',
-      'Chin': 'Chin Gentsai',
-      'Choi': 'Choi Bounge',
-      'Clark': 'Clark Still',
-      'Foxy': 'Foxy',
-      'Daimon': 'Goro Daimon',
-      'Hinako': 'Hinako Shijou',
-      'Iori': 'Iori Yagami',
-      'Jhun': 'Jhun Hoon',
-      'Joe': 'Joe Higashi',
-      'K`': 'K',
-      'K Dash': 'K',
-      'Kasumi': 'Kasumi Todoh',
-      'Kim': 'Kim Kaphwan',
-      'Kula': 'Kula Diamond',
-      'Kyo': 'Kyo Kusanagi',
-      'Leona': 'Leona Heidern',
-      'Xiangfei': 'Li Xiangfei',
+      'Cristiano': 'Cristiano Ronaldo',
+      'Ronaldo': 'Cristiano Ronaldo',
+      'Hotaru': 'Hotaru Futaba',
+      'Kain': 'Kain R. Heinlein',
+      'Kevin': 'Kevin Rian',
+      'Hwan': 'Kim Dong Hwan',
       'Mai': 'Mai Shiranui',
-      'May Lee': 'May Lee(Normal)',
-      'Ralf': 'Ralf Jones',
-      'Robert': 'Robert Garcia',
-      'Ryo': 'Ryo Sakazaki',
-      'Yamazaki': 'Ryuji Yamazaki',
-      'Shingo': 'Shingo Yabuki',
-      'Kensou': 'Sie Kensou',
-      'Takuma': 'Takuma Sakazaki',
+      'Marco': 'Marco Rodrigues',
+      'Rock': 'Rock Howard',
+      'Salvatore': 'Salvatore Ganacci',
+      'Ganacci': 'Salvatore Ganacci',
       'Terry': 'Terry Bogard',
-      'Yashiro': 'Yashiro Nanakase',
-      'Yuri': 'Yuri Sakazaki'
+      'Vox': 'Vox Reaper',
+      'Reaper': 'Vox Reaper'
     };
     if (chart[character] === undefined) {
       return character;
@@ -226,23 +206,24 @@ module.exports = {
   },
   getCharacterImg: function(character) {
     const chartImg = {
-      'EX Kensou': 'kensouex',
-      'EX Robert': 'robertex',
-      'EX Takuma': 'takumaex',
-      'Blue Mary': 'bluemary',
-      'Ryuji Yamazaki': 'yamazaki',
-      'Goro Daimon': 'daimon',
-      'Sie Kensou': 'kensou',
-      'May Lee(Normal)': 'maylee',
-      'May Lee(Hero)': 'maylee',
-      'Li Xiangfei': 'xiangfei',
-      'Orochi Chris': 'orochichris',
-      'Orochi Shermie': 'orochishermie',
-      'Orochi Yashiro': 'orochiyashiro',
+      'Rock Howard': '352409400',
+      'Terry Bogard': '352409401',
+      'Hotaru Futaba': '352409402',
+      'Tizoc': '352409403',
+      'Preecha': '352409404',
+      'Marco Rodrigues': '352409405',
+      'B. Jenet': '352409406',
+      'Vox Reaper': '352409407',
+      'Kevin Rian': '352409408',
+      'Billy Kane': '352409409',
+      'Mai Shiranui': '352409510',
+      'Kim Dong Hwan': '352409511',
+      'Gato': '352409512',
+      'Kain R. Heinlein': '352409513',
+      'Cristiano Ronaldo': '352409514',
+      'Salvatore Ganacci': '370675915',
+      'Hokutomaru': '427039516',
     };
-    if (chartImg[character] === undefined) {
-      return character.toLowerCase().replace('-','').split(' ')[0];
-    }
-    return chartImg[character];
+    return chartImg[character] + '-' + character.toLowerCase().replace('.','').replace(' ','-').replace('ganacci','gannaci');
   }
 };
